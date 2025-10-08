bf77686814b5c4e81ebadc86b75840cd2ee37400
gendis kumala ariga <gendiskumalariga@gmail.com>
2025-10-08 07:45:41 +0700

    percobaan 2

---
+++ Latihan1.java
@@ -0,0 +1,19 @@
+import java .util.Scanner;
+public class Latihan1 {
+    
+    public static void main(String[] args) {
+        String nama;
+        String kelas;
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Siapa nama mu");
+        nama=sc.nextLine();
+        System.out.println("kelas berapa");
+        kelas=sc.nextLine();
+        System.out.println("nama saya adalah " + nama);
+        System.out.println("kelas saya"+kelas);
+
+    
+     }
+
+
+}
